edac723d740810d062d531b906e00b6cc5df261a
mobin <mobinizadi.dev@gmail.com>
2024-09-09 01:21:00 +0330

    fix overflow coupon code

--- src/component/ShoppingCart/PaymentSummary/PaymentSummary.js
+++ src/component/ShoppingCart/PaymentSummary/PaymentSummary.js
@@ -28,8 +28,8 @@ export class PaymentSummary extends Component {
             <div>
                 <h4 className="text-3xl mb-3">Payment Summary</h4>
                 <div className="bg-white rounded-lg  p-3 ">
-                    <div className='flex justify-between flex-wrap gap-1 border-b border-dashed py-6'>
-                        <input type="text" placeholder='COUPON CODE' className='border rounded-lg basis-4/6 p-2 outline-none' onChange={this.offerInputHandler} value={this.state.offerInput} />
+                    <div className='flex justify-center sm:justify-between flex-wrap gap-1 border-b border-dashed py-6'>
+                        <input type="text" placeholder='COUPON CODE' className='border rounded-lg basis-3/6 p-2 outline-none' onChange={this.offerInputHandler} value={this.state.offerInput} />
                         <button className='bg-blue-300 text-blue-600 rounded-lg py-2 px-4 basis-2/6' onClick={this.offerHandler}>Apply</button>
                     </div>
                     <div className=' py-3 space-y-3 border-b border-dashed'>
